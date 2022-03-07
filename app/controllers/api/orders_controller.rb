3ba7765f777095afb2798b4d04d928bff091094a
maxarvid <max.arvid.anderson@gmail.com>
2022-03-07 14:08:25 +0100

    refactors order update action to handle amount in put request

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -5,19 +5,19 @@ class Api::OrdersController < ApplicationController
     product = Product.find(params[:order][:product_id])
     order = Order.create
     order.items.create(product: product, amount: params[:order][:amount])
-    render json: { message: "#{product.name} has been added to your order" }, status: 201
+    render json: { message: "#{product.name} has been added to your order." }, status: 201
   end
 
   def update
     order = Order.find(params[:id])
     product = Product.find(params[:order][:product_id])
-    order.items.create(product: product)
-    render json: { message: "you order is updated" }
+    order.items.create(product: product, amount: params[:order][:amount])
+    render json: { message: "Your order was update with #{product.name}." }
   end
 
   private
 
   def product_not_found
-    render json: { message: "we could not find the product you tried to add." }, status: 404
+    render json: { message: 'we could not find the product you tried to add.' }, status: 404
   end
 end
--- spec/requests/api/orders/update_spec.rb
+++ spec/requests/api/orders/update_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe "PUT /api/orders/", type: :request do
+RSpec.describe 'PUT /api/orders/', type: :request do
   let(:order) { create(:order) }
   let(:product_1) { create(:product) }
   let(:product_2) { create(:product) }
@@ -6,18 +6,18 @@ RSpec.describe "PUT /api/orders/", type: :request do
   subject { response }
 
   before do
-    order.items.create(product: product_2)
+    order.items.create(product: product_2, amount: 1)
     put "/api/orders/#{order.id}",
         params: { order: { product_id: product_1.id, amount: 2 } }
   end
 
   it { is_expected.to have_http_status 200 }
 
-  it "is expected to responed a message" do
-    expect(response_json["message"]).to eq "you order is updated"
+  it 'is expected to responed a message' do
+    expect(response_json['message']).to eq "Your order was update with #{product_2.name}."
   end
 
-  #it "is expected to add amount value to orderItem" do
+  # it "is expected to add amount value to orderItem" do
   # expect(order.items.first.amount).to eq 2
-  #end
+  # end
 end
