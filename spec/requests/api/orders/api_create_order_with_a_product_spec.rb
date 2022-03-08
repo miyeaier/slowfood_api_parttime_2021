ea552882622c2138720d47e02f4d9ba156726493
miyeaier <miyesier@gmail.com>
2022-03-08 21:30:18 +0100

    add new branch auth-demo and add test on api create order with a product spect

--- spec/requests/api/orders/api_create_order_with_a_product_spec.rb
+++ spec/requests/api/orders/api_create_order_with_a_product_spec.rb
@@ -2,35 +2,47 @@ RSpec.describe "POST /api/orders", type: :request do
   let!(:product) { create(:product) }
   subject { response }
 
-  before do
-    post "/api/orders",
-         params: { order: { product_id: product.id, amount: 2 } }
-    @order = Order.last
-  end
+  describe "with valid params" do
+    before do
+      post "/api/orders",
+           params: { order: { product_id: product.id, amount: 2 } }
+      @order = Order.last
+    end
 
-  it { is_expected.to have_http_status 201 }
+    it { is_expected.to have_http_status 201 }
 
-  it "is expected to save order to database" do
-     expect(@order).to_not eq nil
-  end
+    it "is expected to save order to database" do
+      expect(@order).to_not eq nil
+    end
 
-  it "is expected to associate order with an orderItem instance that points to a product" do
-     expect(@order.items.first.product_id).to eq product.id
+    it "is expected to associate order with an orderItem instance that points to a product" do
+      expect(@order.items.first.product_id).to eq product.id
+    end
+
+    it "is expected to associate a product with the order" do
+      expect(@order.products).to include product
+    end
+
+    it "is expected to add amount value to orderItem" do
+      expect(@order.items.first.amount).to eq 2
+    end
+
+    it "is expected to return a message that the product was added to the order" do
+      expect(response_json["message"]).to eq "#{product.name} has been added to your order."
+    end
   end
   
-  it "is expected to associate a product with the order" do
-     expect(@order.products).to include product
-  end
+  describe"without valid params" do
+    before do
+      post "/api/orders",
+           params: { order: { product_id: product.id, amount: 2 } }
+           headers:{}
+    end
+    it{ is_expected.to have_http_status 401}
+  end 
 
-  it "is expected to add amount value to orderItem" do
-     expect(@order.items.first.amount).to eq 2
+    
   end
-
-  it "is expected to return a message that the product was added to the order" do
-     expect(response_json["message"])
-    .to eq "#{product.name} has been added to your order."
-  end
-
   describe "unsuccessful request with invalid product id" do
     before do
       post "/api/orders",
@@ -42,9 +54,7 @@ RSpec.describe "POST /api/orders", type: :request do
     it { is_expected.to have_http_status 404 }
 
     it "is expected to return product not found message" do
-       expect(response_json["message"])
-      .to eq "we could not find the product you tried to add."
+      expect(response_json["message"]).to eq "we could not find the product you tried to add."
     end
-
   end
 end
